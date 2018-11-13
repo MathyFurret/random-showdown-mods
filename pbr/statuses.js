f7ffe3b1f43fed25e09b43f8bbd13b35b8c6ec1c
Alexander B <mathfreak231@users.noreply.github.com>
2018-11-12 18:19:09 -0600

    PBR: Update Fog

--- pbr/statuses.js
+++ pbr/statuses.js
@@ -9,18 +9,25 @@ let BattleStatuses = {
     effectType: 'Weather',
     duration: 0,
     onStart: function() {
-      this.add('-message', 'The fog is deep...');
-      this.add('-weather', 'Fog');
+      this.add('-message', 'The fog is deep... (placeholder)');
+      // this.add('-weather', 'Fog');
     },
     onModifyAccuracy: function (accuracy) {
       if (typeof accuracy !== 'number') return;
       this.debug('Fog modify accuracy');
       return accuracy * (3/5);
     },
+    onHit: function(target, source, move) {
+      if (move.id === 'defog') {
+        this.clearWeather();
+        this.add('-message', `${source.name} blew away the deep fog with Defog! (placeholder)`);
+        // this.add('-weather', 'none', '[from] move: Defog', '[of] ' + source);
+      }
+    },
     onResidualOrder: 1,
     onResidual: function() {
-      this.add('-message', 'The fog is deep...');
-      this.add('-weather', 'Fog', '[upkeep]');
+      this.add('-message', 'The fog is deep... (placeholder)');
+      // this.add('-weather', 'Fog', '[upkeep]');
     },
   },
 };
